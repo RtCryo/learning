879cc7cb625115b6bc58e792569c960b7e720af5
RtCryo <46868152+rtcryo@users.noreply.github.com>
2019-07-07 20:02:58 +0200

    update

--- src/main/java/com/lessons/App.java
+++ src/main/java/com/lessons/App.java
@@ -4,10 +4,34 @@ package com.lessons;
  * Hello world!
  *
  */
-public class App 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import org.apache.log4j.Logger;
+
+public class App extends HttpServlet
 {
-    public static void main( String[] args )
+   // private Logger logger = Logger.getLogger(this.getClass());
+    private RequestDispatcher jsp;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException
     {
-        System.out.println( "Hello World!" );
+        ServletContext context = config.getServletContext();
+        jsp = context.getRequestDispatcher("/testjsp.jsp");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+ //       logger.debug("doGet()");
+        jsp.forward(req, resp); 
     }
 }
---
+++ src/main/webapp/testjsp.jsp
@@ -0,0 +1,16 @@
+<%-- 
+    Document   : testjsp
+    Created on : 06.07.2019, 20:23:44
+    Author     : Alex
+--%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!!!</h1>
+    </body>
+</html>
